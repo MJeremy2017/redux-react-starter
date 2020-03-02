35216bc7605b768439a07e8026b7d8109b35f9a3
jeremy.zhang <zhangyue9306@gmail.com>
2020-03-02 11:03:21 +0800

    add vid list

---
+++ src/components/video_list.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import VideoListItem from "./video_list_item";
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map(vid => {
+      return <VideoListItem key={vid.etag} video={vid} />  // property key and video
+  });
+
+  // props are passed in from App
+  return (
+      <ul className="col-md-4 list-group">
+          {videoItems}
+      </ul>
+  );
+};
+
+export default VideoList;
